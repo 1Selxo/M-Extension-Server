9341515cd8ca91edf34ebbc7ef846911d72c0779
ippo-michi <233349366+ippo-michi@users.noreply.github.com>
2026-07-29 13:11:02 +0200

    chore: prepare v1.0.5.1 release

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -46,7 +46,7 @@ sourceSets {
 }
 
 // should be bumped with each stable release
-val m_extension_serverVersion = "v1.0.4.10"
+val m_extension_serverVersion = "v1.0.5.1"
 
 // counts commit count on master
 val m_extension_serverRevision = runCatching {
